5b1f7b46305122ebbf29ceb31f8ff4c2b2c09235
c.perotto <c.perotto@reply.it>
2025-11-27 11:55:41 +0100

    last fix?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,30 +11,11 @@ export default function HomePage() {
         <div className={styles.scrim} aria-hidden="true" />
 
         <header className={styles.content}>
-<<<<<<< HEAD
-          <img src="/assets/img/logo2.png" alt="Logo" className={styles.logo} />
-=======
           <img src="/assets/img/logo3.png" alt="Logo" className={styles.logo} />
->>>>>>> master
         </header>
 
         <nav className={styles.iconGrid} aria-label="Sezioni principali">
           <Link href="/menu-food" className={styles.appIcon}>
-<<<<<<< HEAD
-            <img src="/assets/img/menu1.png" alt="Menu drink icon" className={styles.iconImage}   style={{ marginBottom: '-40px'}}/>
-          </Link>
-
-          <Link href="/menu-drink" className={styles.appIcon}>
-            <img src="/assets/img/menu2.png" alt="Menu drink icon" className={styles.iconImage} />
-          </Link>
-
-          <Link href="/eventi" className={styles.appIcon}>
-           <img src="/assets/img/menu3.png" alt="Menu drink icon" className={styles.iconImage} />
-          </Link>
-
-          <Link href="/chi-siamo" className={styles.appIcon}>
-            <img src="/assets/img/menu4.png" alt="Menu drink icon" className={styles.iconImage} />
-=======
             <img src="/assets/img/foto4.png" alt="Menu drink icon" className={styles.iconImage} />
           </Link>
 
@@ -48,7 +29,6 @@ export default function HomePage() {
 
           <Link href="/chi-siamo" className={styles.appIcon}>
             <img src="/assets/img/foto2.png" alt="Menu drink icon" className={styles.iconImage} />
->>>>>>> master
           </Link>
         </nav>
 
